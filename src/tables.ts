05a14dba7db3cb14b4e49ba59f78b22044dc7df6
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-16 14:15:40 -0500

    new: define tables

---
+++ src/tables.ts
@@ -0,0 +1,76 @@
+import { int, nil, obj, str } from "@libn/json/build";
+
+const table =
+  (name: string) =>
+  ($: TemplateStringsArray, ...columns: (string | [string, string])[]) => {
+    let sql = `CREATE TABLE IF NOT EXISTS ${name} (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  note TEXT NOT NULL DEFAULT "",`;
+    for (let z = 0; z < columns.length; ++z) {
+      const column = columns[z];
+      if (Array.isArray(column)) {
+        sql += `${$[z]}${column[0]} INTEGER NOT NULL REFERENCES ${
+          column[1]
+        }(id) DEFERRABLE INITIALLY DEFERRED`;
+      } else sql += $[z] + column;
+    }
+    return `${sql}${$[columns.length].replace(/,$/, "")}
+);
+CREATE TRIGGER IF NOT EXISTS update_timestamp_${name}
+AFTER UPDATE OF ${columns.join(", ")} ON ${name}
+BEGIN UPDATE ${name} SET updated = CURRENT_TIMESTAMP WHERE id = NEW.id; END;`;
+  };
+export const CREATE = [
+  "PRAGMA foreign_keys = ON;",
+  table("person")`
+  ${"name"} VARCHAR(255),
+  ${"info"} TEXT NOT NULL,`,
+  table("course")`
+  ${"name"} VARCHAR(255),
+  ${"info"} TEXT NOT NULL,`,
+  table("family")`
+  ${["upper", "person"]},
+  ${["lower", "person"]},
+  UNIQUE(upper, lower),`,
+  table("signup")`
+  ${["course", "course"]},
+  ${["person", "person"]},`,
+  table("record")`
+  ${["signup", "signup"]},
+  ${"began"} TIMESTAMP,
+  ${"ended"} TIMESTAMP,`,
+];
+const META = {
+  id: int({ minimum: 0, maximum: 0xffffffff }),
+  created: str({ format: "date-time" }),
+  updated: str({ format: "date-time" }),
+  note: str(),
+};
+export const person = obj({
+  ...META,
+  name: str({ minLength: 1, maxLength: 255 }),
+  info: str({ pattern: "^\\{.*\\}$" }),
+});
+export const course = obj({
+  ...META,
+  name: str({ minLength: 1, maxLength: 255 }),
+  info: str({ pattern: "^\\{.*\\}$" }),
+});
+export const family = obj({
+  ...META,
+  upper: int(),
+  lower: int(),
+});
+export const signup = obj({
+  ...META,
+  course: int(),
+  person: int(),
+});
+export const record = obj({
+  ...META,
+  signup: int(),
+  began: nil(str({ format: "date-time" })),
+  ended: nil(str({ format: "date-time" })),
+});
